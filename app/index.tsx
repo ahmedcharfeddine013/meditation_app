a5fab0472c8e20198f9a7894f9cbaf05a1ad9092
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-07-23 16:54:15 +0100

    homescreen

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,10 @@
-import { View, Text, ImageBackground } from "react-native";
+import { View, Text, ImageBackground, SafeAreaView } from "react-native";
+
+import { StatusBar } from "expo-status-bar";
 
 import beachImage from "@/assets/meditation-images/beach.webp";
+
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
@@ -10,7 +14,23 @@ export default function App() {
         resizeMode="cover"
         className="flex-1"
       >
-        <Text>Home Screen</Text>
+        <LinearGradient
+          className="flex-1"
+          colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}
+        >
+          <SafeAreaView className="flex-1 mx-5 my-12 justify-between">
+            <View>
+              <Text className="text-center text-white font-bold text-4xl">
+                Simple Meditation
+              </Text>
+              <Text className="text-center text-white text-2xl mt-3 text-regular">
+                Simplifying meditation for everyone
+              </Text>
+            </View>
+            <View></View>
+            <StatusBar style="light"></StatusBar>
+          </SafeAreaView>
+        </LinearGradient>
       </ImageBackground>
     </View>
   );
